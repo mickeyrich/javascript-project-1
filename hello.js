10be3b9cb16f9f904dee71b0afeb08addcda9296
mickeyrich <airclayton@hotmail.com>
2023-10-26 13:03:05 -0400

    JS Assignment 1 HmWk

---
+++ hello.js
@@ -0,0 +1,43 @@
+alert("Hello, World!");
+
+// Prompt the user for their name and store it in the 'username' variable
+var username = prompt("What's your name?");
+
+// Create a friendly message with the user's name
+var message = "Hello, " + username + "! How are you feeling today?";
+
+// Display the message using the 'alert' function
+alert(message);
+
+// -----------------------
+
+// Prompt the user for their name and store it in the 'username' variable
+var username = prompt("What's your name?");
+
+// Prompt the user for their age and store it in the 'age' variable
+var age = parseFloat(prompt("How old are you?"));
+
+// Prompt the user for their choice (one or two messages)
+var choice = prompt(
+  "Do you want one message or two messages? (Type 'one' or 'two')"
+);
+
+// Create a friendly message with the user's name, age, and math-based message
+var message = "Hello, " + username + "!";
+
+if (!isNaN(age)) {
+  message += " You are " + age + " years old.";
+} else {
+  message += " Your age is not specified.";
+}
+
+if (choice === "one") {
+  message += " How are you feeling today?";
+} else if (choice === "two") {
+  message += " Thanks for visiting. Have a great day!";
+} else {
+  message += " We're not sure what you want, but have a great day!";
+}
+
+// Display the message using the 'alert' function
+alert(message);
