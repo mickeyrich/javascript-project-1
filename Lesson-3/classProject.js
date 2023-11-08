f7d8568cf59bbc37a473d25b75d9b50b5af1ce36
mickeyrich <airclayton@hotmail.com>
2023-11-07 22:31:59 -0500

    Updates

--- Lesson-3/classProject.js
+++ Lesson-3/classProject.js
@@ -1,3 +1,5 @@
+let imageCount = 0;
+
 document
   .getElementById("imageForm")
   .addEventListener("submit", function (event) {
@@ -7,10 +9,39 @@ document
 
     document.getElementById("imageUrl").value = "";
 
-    const img = document.createElement("img");
-    img.src = imageUrl.trim();
-    img.style.width = "600px";
+    if (isValidURL(imageUrl)) {
+      imageCount++;
 
-    const imageContainer = document.getElementById("imageContainer");
-    imageContainer.appendChild(img);
+      document.getElementById("imageCount").textContent = imageCount;
+
+      const img = document.createElement("img");
+      img.src = imageUrl.trim();
+      img.style.width = "600px";
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function () {
+        img.remove();
+        deleteButton.remove();
+
+        imageCount--;
+
+        document.getElementById("imageCount").textContent = imageCount;
+      });
+
+      const imageContainer = document.getElementById("imageContainer");
+      imageContainer.appendChild(img);
+      imageContainer.appendChild(deleteButton);
+    } else {
+      alert("Please enter a valid image URL.");
+    }
   });
+
+function isValidURL(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
